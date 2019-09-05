e68edc69d2b11ad3613e89feb39ed3b1c86ac424
Johan Swanepoel <johanswanepoel@Johans-MacBook-Pro.local>
2019-09-05 20:15:45 +1000

    Changed to war

--- src/main/java/com/example/provider/ProviderApplication.java
+++ src/main/java/com/example/provider/ProviderApplication.java
@@ -1,10 +1,16 @@
 package com.example.provider;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProviderApplication {
+public class ProviderApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProviderApplication.class)
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProviderApplication.class, args);

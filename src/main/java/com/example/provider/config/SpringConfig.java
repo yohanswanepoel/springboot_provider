d6871b4aa317735738052a27d2857b9458146955
Johan Swanepoel <johannes.swanepoel@gmail.com>
2019-09-06 15:00:20 +1000

    Providers APIs springboot working

---
+++ src/main/java/com/example/provider/config/SpringConfig.java
@@ -0,0 +1,23 @@
+package com.example.provider.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import com.example.provider.controller.ProviderController;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses = ProviderController.class)
+@Import(SwaggerConfiguration.class)
+public class SpringConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
---
+++ src/main/java/com/example/provider/controller/ProviderController.java
@@ -0,0 +1,92 @@
+package com.example.provider.controller;
+
+import com.example.provider.exception.ResourceNotFoundException;
+import com.example.provider.model.Provider;
+import com.example.provider.repository.ProviderRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+@Api(value = "Provider Management APIs", description = "Operations to manage providers and their onboarding status")
+public class ProviderController {
+    
+    @Autowired
+    private ProviderRepository providerRepository;
+
+
+    @GetMapping("/health")
+    public String demo() {
+        System.out.println("Healthy");
+        return "{\"status\":\"healthy\"}";
+    }
+
+    /**
+     * Get all providers list
+     */
+    @ApiOperation(value = "View a list of available providers", response = List.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved list"),
+        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+    @GetMapping("/providers")
+    public List<Provider> getAllProviders(){
+        return providerRepository.findAll();
+    }
+
+    /**
+     * get providers by id
+     */
+    @ApiOperation(value = "Get provider by ID")
+    @GetMapping("/providers/{id}")
+    public ResponseEntity<Provider> getProviderById(
+        @ApiParam(value = "Provider id from which provider object will retrieved", required = true)
+        @PathVariable(value = "id") Long providerId)
+        throws ResourceNotFoundException {
+            Provider provider = 
+                providerRepository
+                    .findById(providerId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Provider Not Found"));
+            return ResponseEntity.ok().body(provider);
+    }
+
+    /**
+   * Create provider.
+   */
+    @ApiOperation(value = "Add a provider")
+    @PostMapping("/providers")
+    public Provider createProvider(
+        @ApiParam(value = "Provider object store in database table", required = true)
+        @Valid @RequestBody Provider provider) {
+        return providerRepository.save(provider);
+    }
+
+    @ApiOperation(value = "Delete a provider a provider")
+    @DeleteMapping("/providers/{id}")
+    public Map<String, Boolean> deleteProvider(@PathVariable(value = "id") Long providerId) throws Exception {
+        Provider provider =
+            providerRepository
+                .findById(providerId)
+                .orElseThrow(() -> new ResourceNotFoundException("Provider not found on :: " + providerId));
+        providerRepository.delete(provider);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+
+}
